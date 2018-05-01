e18cbe685ce35e48f491371082f6d69cd9d4b058
Subhaam <derksoul98@gmail.com>
2018-05-01 11:24:45 +0530

    build.sh: changes for magma
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- AnyKernel2/anykernel.sh
+++ AnyKernel2/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=FireKernel by FireLord @ xda-developers
+kernel.string=MagmaKernel by Subham @ xda-developers
 do.devicecheck=1
 do.modules=1
 do.cleanup=1
