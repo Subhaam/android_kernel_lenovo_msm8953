566d20d66b6a72e3ecab5209e18ebd100f1f0775
Subhaam <derksoul98@gmail.com>
2018-05-25 21:29:21 +0530

    build_Script: crankycoconut
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ nocol='\033[0m'
 
 # Kernel details
 KERNEL_NAME="MagmaKernel"
-VERSION="sizzlingnuts"
+VERSION="crankycoconut"
 DATE=$(date +"%d-%m-%Y-%I-%M")
 DEVICE="kuntao"
 FINAL_ZIP=$KERNEL_NAME-$VERSION-$DEVICE-$DATE.zip
